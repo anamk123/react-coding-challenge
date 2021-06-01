f0a937b944916933ba0c9ccf15621a52b320898e
anamk123 <anamk1994@gmail.com>
2021-06-01 14:15:08 +1200

    get request for new-release

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,71 @@
-import React, {useState,} from "react";
+import React, {useState, useEffect} from "react";
 import axios from 'axios';
 
 
+
 function Home(props){
+    const spotifyClientId = process.env.REACT_APP_CLIENT_ID;
+    const spotifySecret = process.env.REACT_APP_CLIENT_SECRET;
+
+    const [data, setData] = useState(null);
+    const [token, setToken] = useState(null);
+
+    useEffect(() => {
+
+        axios('https://accounts.spotify.com/api/token', {
+          headers: {
+            'Content-Type' : 'application/x-www-form-urlencoded',
+            'Authorization' : 'Basic ' + btoa(spotifyClientId + ':' + spotifySecret)      
+          },
+          data: 'grant_type=client_credentials',
+          method: 'POST'
+        })
+        .then(tokenResponse => {      
+          setToken(tokenResponse.data.access_token);
+    
+          axios('https://api.spotify.com/v1/browse/new-releases?country=NZ&limit=10&offset=5'
+            , {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + tokenResponse.data.access_token}
+          })
+          .then(response => setData(response.data));
+          
+        });
+    
+      }, [spotifyClientId, spotifySecret]);
+
 
     return(
         <div>
-            hill
+              {!data ?  (
+                            <div>
+                                Loading
+                            </div>
+
+                            ) : (
+                            
+                            <div className="holdingContainer">
+                              
+                               {data['albums']['items'].map((data, index) => {
+                                return(
+                                    <>
+                                    <div className="items" key={index}>
+                                        <img src={data['images'][1]['url']}></img>
+                                        <p key={index}>{data['name']} <br></br> {data['artists'][0]['name']} {console.log(data)}</p>
+
+                                    </div>
+
+                                </>
+                                
+                                
+                                
+                                
+                                )
+                    })}
+                            </div>
+                            )} 
+            
+            
         </div>
     )
 }
