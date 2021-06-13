5e0c999e3dcfd09e6642f981c278d4a8dcc90ee9
anamk123 <anamk1994@gmail.com>
2021-06-13 22:06:09 +1200

    gettign click functions

--- client/src/components/layout/categories.js
+++ client/src/components/layout/categories.js
@@ -4,6 +4,8 @@ import React, { useState, useEffect } from "react";
 function Categories(props){
     
     const [categories, setCategories] = useState("");
+    const[name, setName] = useState("");
+    const [image, setImage] = useState("")
     
 
       useEffect(() => {
@@ -11,28 +13,32 @@ function Categories(props){
         .then(response => setCategories(response.data));
       }, [setCategories]);
 
+
+      
+      
+
 return(
 
     <>
 <>
+
 <div {...props.title}></div>
 
                             {!categories ?  (
                             
                             <div>
-                                Loading {console.log(categories)}
+                                Loading 
                             </div>
                             ) : (
                                 
                                 <div className='flex' style={{display: 'flex', padding : '10px', marginLeft: '10px'}}>
-                                    {console.log(categories)}
-                                      {console.log(categories.categories.items)}
-                                      {/* {console.log(featured.description)} */}
-
+                                  
+                                  <input type='text' value={name}></input>
+                                   <input type='text' value={image}></input>
                                 {categories.categories.items.map((data, index) => {
                                 return(
 
-                                    <div className="items" style={{margin: '15px'}} key={index}>
+                                    <div className="items" style={{margin: '15px'}} key={index} id={data['name']} value={data.icons[0].url} >
 
                                         <p key={index}><br></br>Playlist: {data['name']} <br></br> <img src={data.icons[0].url}></img> </p>
                                     </div>)
