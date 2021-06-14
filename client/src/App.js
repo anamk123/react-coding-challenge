462cb7c4b684109ff132b940970d37d47580783f
Anam <anam.khan@wove.co.nz>
2021-06-15 11:45:39 +1200

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,16 @@ import Router from "./Router";
 import axios from "axios";
 import { AuthContextProvider } from "./context/AuthContext";
 
+// import React, { useState, useEffect } from "react";
+
+
+
 axios.defaults.withCredentials = true;
 
 function App() {
+//   useEffect(() => {
+//     keepTheme();
+// })
   return (
     <AuthContextProvider>
       <Router />
--- client/src/Router.js
+++ client/src/Router.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
-import Customers from "./components/customers/Customers";
+
 import Navbar from "./components/layout/Navbar";
 import AuthContext from "./context/AuthContext";
 import Home from './components/layout/Home';
